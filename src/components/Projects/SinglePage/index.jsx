87273527259a11c634540383ab3d6742e8176230
Jerome-Baille <baille.jerome@gmail.com>
2022-07-20 19:17:29 +0200

    fetching projects data from an outside source

--- src/components/Projects/SinglePage/index.jsx
+++ src/components/Projects/SinglePage/index.jsx
@@ -4,10 +4,28 @@ import AnimatedLetters from '../../AnimatedLetters';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faInternetExplorer } from "@fortawesome/free-brands-svg-icons";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
-import data from '../../../assets/en.json'
 import Loader from "react-loaders";
 
 const ProjectSinglePage = () => {
+    const [projects, setProjects] = useState([])
+  
+    useEffect(() => {
+      const urls = [
+          "https://jerome-baille-portfolio.herokuapp.com/api/projects"
+        ];
+  
+      const getData = async () => {
+        const [projects] = await Promise.all(
+          urls.map((url) => fetch(url)
+            .then((res) => res.json()))
+       );
+        setProjects(projects);
+      };
+  
+      getData();
+    }, [])
+
+
     const { id } = useParams()
     const navigate = useNavigate();
 
@@ -19,96 +37,98 @@ const ProjectSinglePage = () => {
         }, 4000)
     }, [])
 
-    return (
-        <>
-        <div className="container">
-            <section className="single-project">
-                <div className="text-column">
-                    <button onClick={() =>navigate(-1)} className='flat-button mr0 back-left'>
-                        <FontAwesomeIcon icon={faArrowLeft} />
-                        <span> Back</span>
-                    </button>
-                    <h1>
-                        <AnimatedLetters
-                            letterClass={letterClass}
-                            strArray={Object.values(data.Projects[id].name)}
-                            idx={15}
-                        />
-                    </h1> 
-                    <div className="subtitle">
-                        <h2>{data.Projects[id].subtitle}</h2>
+    if(projects.length !== 0) {
+        return (
+            <>
+            <div className="container">
+                <section className="single-project">
+                    <div className="text-column">
+                        <button onClick={() =>navigate(-1)} className='flat-button mr0 back-left'>
+                            <FontAwesomeIcon icon={faArrowLeft} />
+                            <span> Back</span>
+                        </button>
+                        <h1>
+                            <AnimatedLetters
+                                letterClass={letterClass}
+                                strArray={Object.values(projects[id].name)}
+                                idx={15}
+                            />
+                        </h1> 
+                        <div className="subtitle">
+                            <h2>{projects[id].subtitle}</h2>
+                        </div>
+                        {projects[id].descriptions.map((description, index) => (
+                            <p key={index}>
+                                {description}
+                            </p>
+                        ))}
+
+                        <section className="skills">
+                            <h3>
+                                Needed Skills
+                            </h3>
+
+                            <ul>
+                                {projects[id].skills.map((skill, index) => (
+                                    <li key={index}>
+                                        {skill}
+                                    </li>
+                                ))}
+                            </ul>
+                        </section>
+
+                        <section className="stack">
+                            <h3>
+                                Stack Used
+                            </h3>
+                            <div className="stack-container">
+                                {projects[id].stack.map((stack, index) => (
+                                    <div className="stack-single" key={index}>
+                                        <img src={process.env.PUBLIC_URL + stack.url} alt={stack.name} />                            
+                                        <span>{stack.name}</span>
+                                    </div>
+                                ))}
+                            </div>
+                        </section>
+                        <section className="improvement">
+                            <h3>
+                                Possible areas of improvement
+                            </h3>
+                            <ul>
+                                {projects[id].improvements.map((improvement, id) => (
+                                    <li key={id}>
+                                        {improvement}
+                                    </li>
+                                ))}
+                            </ul>
+                        </section>
                     </div>
-                    {data.Projects[id].descriptions.map((description, index) => (
-                        <p key={index}>
-                            {description}
-                        </p>
-                    ))}
+                    <div className="img-column">
+                        <button onClick={() =>navigate(-1)} className='flat-button mr0 back-right'>
+                            <FontAwesomeIcon icon={faArrowLeft} />
+                            <span> Back</span>
+                        </button>
+                        {projects[id].pictures.map((picture, index) => (
+                            <div className="img-wraper" key={index}>
+                                <img src={process.env.PUBLIC_URL + picture.url} alt={picture.alt}/>
+                            </div>
+                        ))}
 
-                    <section className="skills">
-                        <h3>
-                            Needed Skills
-                        </h3>
-
-                        <ul>
-                            {data.Projects[id].skills.map((skill, index) => (
-                                <li key={index}>
-                                    {skill}
-                                </li>
-                            ))}
-                        </ul>
-                    </section>
-
-                    <section className="stack">
-                        <h3>
-                            Stack Used
-                        </h3>
-                        <div className="stack-container">
-                            {data.Projects[id].stack.map((stack, index) => (
-                                <div className="stack-single" key={index}>
-                                    <img src={process.env.PUBLIC_URL + stack.url} alt={stack.name} />                            
-                                    <span>{stack.name}</span>
-                                </div>
-                            ))}
-                        </div>
-                    </section>
-                    <section className="improvement">
-                        <h3>
-                            Possible areas of improvement
-                        </h3>
-                        <ul>
-                            {data.Projects[id].improvements.map((improvement, id) => (
-                                <li key={id}>
-                                    {improvement}
-                                </li>
-                            ))}
-                        </ul>
-                    </section>
-                </div>
-                <div className="img-column">
-                    <button onClick={() =>navigate(-1)} className='flat-button mr0 back-right'>
-                        <FontAwesomeIcon icon={faArrowLeft} />
-                        <span> Back</span>
-                    </button>
-                    {data.Projects[id].pictures.map((picture, index) => (
-                        <div className="img-wraper" key={index}>
-                            <img src={process.env.PUBLIC_URL + picture.url} alt={picture.alt}/>
-                        </div>
-                    ))}
-
-                    <footer>
-                        <a href={data.Projects[id].github} target="_blank" rel="noopener noreferrer" className="github-link">
-                            <FontAwesomeIcon icon={faGithub} />
-                        </a>
-                        {data.Projects[id].demo? <a href={data.Projects[id].demo} target="_blank" rel="noopener noreferrer" className="demo-link">
-                            <FontAwesomeIcon icon={faInternetExplorer} />
-                        </a> : null}
-                    </footer>
-                </div>
-            </section>
-        </div>
-        <Loader type="pacman"/>
-        </>
-    )
+                        <footer>
+                            <a href={projects[id].github} target="_blank" rel="noopener noreferrer" className="github-link">
+                                <FontAwesomeIcon icon={faGithub} />
+                            </a>
+                            {projects[id].demo? <a href={projects[id].demo} target="_blank" rel="noopener noreferrer" className="demo-link">
+                                <FontAwesomeIcon icon={faInternetExplorer} />
+                            </a> : null}
+                        </footer>
+                    </div>
+                </section>
+            </div>
+            <Loader type="pacman"/>
+            </>
+        )
+    }
 }
 
 export default ProjectSinglePage;
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -3,11 +3,28 @@ import AnimatedLetters from '../AnimatedLetters';
 import Loader from 'react-loaders'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faInternetExplorer } from "@fortawesome/free-brands-svg-icons";
-import data from '../../assets/en.json'
 import { Link } from 'react-router-dom';
 
 
 const Projects = () => {
+    const [projects, setProjects] = useState([])
+
+    useEffect(() => {
+        const urls = [
+            "https://jerome-baille-portfolio.herokuapp.com/api/projects"
+          ];
+    
+        const getData = async () => {
+          const [projects] = await Promise.all(
+            urls.map((url) => fetch(url)
+              .then((res) => res.json()))
+         );
+         setProjects(projects);
+        };
+    
+        getData();
+      }, [])
+
     const [letterClass, setLetterClass] = useState('text-animate');
 
     useEffect(() => {
@@ -16,57 +33,59 @@ const Projects = () => {
         }, 4000)
     }, [])
 
-    return (
-        <>
-        <div className='container projects-page'>
-            <section className='project-section'>
-                <h1>
-                    <AnimatedLetters
-                        letterClass={letterClass}
-                        strArray={['M', 'y', ' ', 'C', 'u', 'r', 'a', 't', 'e', 'd', ' ', 'P', 'r', 'o', 'j', 'e', 'c', 't', 's']}
-                        idx={15}
-                    />
-                </h1>
-                <p>
-                    A small collection of recent projects I have worked on to get a better understanding of my skills and abilities.
-                    I have done them all together with amazin people. Please note that with the exception of the Groupomania project, I did not create the design of the website.
-                </p>
-                <div className="projects-wrapper">
-                {Object.keys(data.Projects).map((project, index) => (
-                    <div className="flip-card-container" key={index}>
-                        <div className="flip-card">
-                            <div className="flip-card-front">
-                                <img src={process.env.PUBLIC_URL + data.Projects[project].logo} alt={data.Projects[project].name + ' logo'} />
-                            </div>
-                            <div className="flip-card-back">
-                                <h3>
-                                    {data.Projects[project].name}
-                                </h3>
-                                <p>{data.Projects[project].subtitle}</p>
-                                <p>{data.Projects[project].tags}</p>
-                                <div className='card-footer'>
-                                    <a href={data.Projects[project].github} target="_blank" rel="noopener noreferrer" className='github-link' aria-label={data.Projects[project].name + ' GitHub'}>
-                                        <FontAwesomeIcon icon={faGithub} />
-                                    </a>
-
-                                    {data.Projects[project].demo? 
-                                        <a href={data.Projects[project].demo} target="_blank" rel="noopener noreferrer" className='demo-link' aria-label={data.Projects[project].name + ' live demo'}>
-                                            <FontAwesomeIcon icon={faInternetExplorer} />
-                                        </a> 
-                                        : null 
-                                    }
+    if(projects.length !== 0){
+        return (
+            <>
+            <div className='container projects-page'>
+                <section className='project-section'>
+                    <h1>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={['M', 'y', ' ', 'C', 'u', 'r', 'a', 't', 'e', 'd', ' ', 'P', 'r', 'o', 'j', 'e', 'c', 't', 's']}
+                            idx={15}
+                        />
+                    </h1>
+                    <p>
+                        A small collection of recent projects I have worked on to get a better understanding of my skills and abilities.
+                        I have done them all together with amazin people. Please note that with the exception of the Groupomania project, I did not create the design of the website.
+                    </p>
+                    <div className="projects-wrapper">
+                    {Object.keys(projects).map((project, index) => (
+                        <div className="flip-card-container" key={index}>
+                            <div className="flip-card">
+                                <div className="flip-card-front">
+                                    <img src={process.env.PUBLIC_URL + projects[project].logo} alt={projects[project].name + ' logo'} />
                                 </div>
-                                <Link to={`/projects/${data.Projects[project].id}`} className="card-link" aria-label={'See more about ' + data.Projects[project].name} />
+                                <div className="flip-card-back">
+                                    <h3>
+                                        {projects[project].name}
+                                    </h3>
+                                    <p>{projects[project].subtitle}</p>
+                                    <p>{projects[project].tags}</p>
+                                    <div className='card-footer'>
+                                        <a href={projects[project].github} target="_blank" rel="noopener noreferrer" className='github-link' aria-label={projects[project].name + ' GitHub'}>
+                                            <FontAwesomeIcon icon={faGithub} />
+                                        </a>
+
+                                        {projects[project].demo? 
+                                            <a href={projects[project].demo} target="_blank" rel="noopener noreferrer" className='demo-link' aria-label={projects[project].name + ' live demo'}>
+                                                <FontAwesomeIcon icon={faInternetExplorer} />
+                                            </a> 
+                                            : null 
+                                        }
+                                    </div>
+                                    <Link to={`/projects/${projects[project].id}`} className="card-link" aria-label={'See more about ' + projects[project].name} />
+                                </div>
                             </div>
                         </div>
+                    ))}
                     </div>
-                ))}
-                </div>
-            </section>
-        </div>
-        <Loader type="pacman"/>
-        </>
-    )
+                </section>
+            </div>
+            <Loader type="pacman"/>
+            </>
+        )
+    }
 }
 
 export default Projects
