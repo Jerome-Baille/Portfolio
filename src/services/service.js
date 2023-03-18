3854310f624fec389fc8d833a1cacd6246722d17
Jerome-Baille <baille.jerome@gmail.com>
2023-03-18 18:49:28 +0100

    Refactor services file

--- src/services/service.js
+++ src/services/service.js
@@ -1,77 +1,52 @@
 // const baseURL = 'https://backend-portfolio.onrender.com/api/';
-const baseURL = 'https://jerome-baille.fr/api/';
-// const baseURL = 'http://localhost:3000/api/';
+// const baseURL = 'https://jerome-baille.fr/api/';
+const baseURL = 'http://localhost:3000/api/';
 
 
 export async function getAbout() {
-  const urlAbout = [`${baseURL}about`];
-  const [about] = await Promise.all(
-    urlAbout.map((url) => fetch(url)
-      .then((res) => res.json()))
- );
+  const urlAbout = `${baseURL}about`;
+  const about = await fetch(urlAbout).then((res) => res.json());
   return about;
-};
+}
 
 export async function getStack() {
-  const urlStacks = [`${baseURL}stacks`];
-  const [stack] = await Promise.all(
-    urlStacks.map((url) => fetch(url)
-      .then((res) => res.json()))
- );
+  const urlStacks = `${baseURL}stacks`;
+  const stack = await fetch(urlStacks).then((res) => res.json());
   return stack;
-};
+}
 
 export async function getAllProjects() {
-  const urlProjects = [`${baseURL}projects`];
-  const [projects] = await Promise.all(
-    urlProjects.map((url) => fetch(url)
-      .then((res) => res.json()))
- );
+  const urlProjects = `${baseURL}projects`;
+  const projects = await fetch(urlProjects).then((res) => res.json());
   return projects;
-};
+}
 
 export async function getOneProject(id) {
-  const urlProject = [`${baseURL}projects/${id}`];
-  const [project] = await Promise.all(
-    urlProject.map((url) => fetch(url)
-      .then((res) => res.json()))
- );
+  const urlProject = `${baseURL}projects/${id}`;
+  const project = await fetch(urlProject).then((res) => res.json());
   return project;
-};
-
+}
 
 export async function getCertifications() {
-  const urlCertifications = [`${baseURL}certifications`];
-  const [certifications] = await Promise.all(
-    urlCertifications.map((url) => fetch(url)
-      .then((res) => res.json()))
- );
+  const urlCertifications = `${baseURL}certifications`;
+  const certifications = await fetch(urlCertifications).then((res) => res.json());
   return certifications;
-};
+}
 
 export async function getChallenges() {
-  const urlChallenges = [`${baseURL}challenges`];
-  const [challenges] = await Promise.all(
-    urlChallenges.map((url) => fetch(url)
-      .then((res) => res.json()))
- );
+  const urlChallenges = `${baseURL}challenges`;
+  const challenges = await fetch(urlChallenges).then((res) => res.json());
   return challenges;
-};
+}
 
 export async function getHundredDaysOfCSS() {
-  const urlHundredDaysOfCSS = [`${baseURL}challenges/DaysOfCSS`];
-  const [DaysOfCSS] = await Promise.all(
-    urlHundredDaysOfCSS.map((url) => fetch(url)
-      .then((res) => res.json()))
- );
+  const urlHundredDaysOfCSS = `${baseURL}challenges/DaysOfCSS`;
+  const DaysOfCSS = await fetch(urlHundredDaysOfCSS).then((res) => res.json());
   return DaysOfCSS;
-};
+}
 
 export async function getTrainings() {
-  const urlTrainings = [`${baseURL}trainings`];
-  const [trainings] = await Promise.all(
-    urlTrainings.map((url) => fetch(url)
-      .then((res) => res.json()))
- );
+  const urlTrainings = `${baseURL}trainings`;
+  const trainings = await fetch(urlTrainings).then((res) => res.json());
   return trainings;
-};
+}
