33a6e66a24b89c55744cbda1a9dd002addf18ffd
Jerome-Baille <baille.jerome@gmail.com>
2023-04-20 23:36:03 +0200

    Add alert message when the contact form is submitted

--- src/features/Contact/ContactForm/ContactFormContainer.js
+++ src/features/Contact/ContactForm/ContactFormContainer.js
@@ -1,7 +1,25 @@
+import React, { useState } from 'react';
 import emailjs from '@emailjs/browser'
 import ContactForm from '../../../components/CustomForm'
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import Snackbar from '@mui/material/Snackbar';
 
 const ContactFormContainer = ({ formData }) => {
+    const [showAlert, setShowAlert] = useState(false);
+    const [state, setState] = useState({
+        open: false,
+        vertical: 'top',
+        horizontal: 'center',
+        type: 'success'
+      });
+    const { vertical, horizontal, open, type } = state;
+    
+    const toggleToast = (type) => {
+        setState({ ...state, open: !open, type });
+        setShowAlert(!showAlert);
+    };
+
     const handleFormSubmit = (values) => {
         emailjs
         .send(
@@ -12,20 +30,39 @@ const ContactFormContainer = ({ formData }) => {
         )
         .then(
             () => {
-            alert('Message successfully sent!')
-            window.location.reload(false)
+                toggleToast('success');
             },
             () => {
-            alert('Failed to send the message, please try again')
+                toggleToast('error');
             }
         )
     };
 
     return (
-        <ContactForm 
-            formData={formData} 
-            onSubmit={handleFormSubmit}
-        />
+        <>
+            {showAlert && (
+                <Snackbar 
+                    anchorOrigin={{ vertical, horizontal }}
+                    open={open} 
+                    autoHideDuration={4000} 
+                    onClose={toggleToast}
+                    key={{ vertical, horizontal }}
+                >
+                    <Alert  
+                        onClose={toggleToast} 
+                        severity={type} 
+                        sx={{ width: '100%' }}
+                    >
+                        <AlertTitle>{type === 'success' ? formData.toast.success.title : formData.toast.error.title}</AlertTitle>
+                        {type === 'success' ? formData.toast.success.message : formData.toast.error.message}
+                    </Alert>
+                </Snackbar>
+            )}
+            <ContactForm 
+                formData={formData} 
+                onSubmit={handleFormSubmit}
+            />
+        </>
     )
 }
 
